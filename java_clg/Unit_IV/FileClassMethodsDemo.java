4b09d97d6484cb3954778b3b4f4b796b7776b8a8
aayaancoder1 <aayaan.shahwaiz@gmail.com>
2025-10-27 14:39:56 +0530

    File Class Methods

---
+++ java_clg/Unit_IV/FileClassMethodsDemo.java
@@ -0,0 +1,17 @@
+import java.io.*;
+public class FileClassMethodsDemo
+{
+	public static void main(String args[]) throws FileNotFoundException, IOException
+	{
+		File f = new File("Source.txt");
+
+		System.out.println("Empty: " +f.exists());
+		System.out.println("Length: " +f.length());
+		System.out.println("Read: " +f.canRead());
+		System.out.println("Write: " +f.canWrite());
+		System.out.println("Directory: " +f.isDirectory());
+		System.out.println("Name: " +f.getName());
+		System.out.println("Path: " +f.getPath());
+		System.out.println("Last Modified: " +f.lastModified());
+	}
+}
